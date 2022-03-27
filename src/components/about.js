8cdccdc3cadc31561d482b196b639e3d4a3b5d15
Tanman6 <tanmansyx@protonmail.com>
2022-03-27 18:36:36 -0500

    init commit

---
+++ src/components/about.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => (
+        <section>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <h2>TrueCoders Team</h2>
+    <p>This site is brought to you by the TrueCoders Team</p>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <ol>
+        <h2><a href="mailto:ejheath8888@gmail.com"> Elijah Heath</a> </h2>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h2><a href="mailto:tanmansyx@protonmail.com"> Tanner Syx </a> </h2>
+    </ol>
+    </section>
+    );
+
+export default About;
