d2e02f3428d6f50be39eba6bebee323f9b69d04e
Tanman6 <tanmansyx@protonmail.com>
2022-04-03 22:10:05 -0500

    updated NewNav

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import { Container } from "react-bootstrap";
 
-import { NavBar, Footer, Loading} from "./components";
-import { Home, Profile, ExternalApi, About, MyAvailability, MyEvents } from "./views";
+import { NewNav, Footer, Loading} from "./components";
+
 import { useAuth0 } from "@auth0/auth0-react";
 
 import "./main.css";
@@ -16,17 +14,7 @@ const App = () => {
   }
   return (
     <div id="app" className="d-flex flex-column h-100">
-      <NavBar />
-      <Container className="flex-grow-1 mt-5">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/external-api" component={ExternalApi} />
-          <Route path="/my-availability" component={MyAvailability} />
-          <Route path="/my-events" component={MyEvents} />
-          <Route path="/about" component={About} />
-        </Switch>
-      </Container>
+      <NewNav />
       <Footer />
     </div>
   );
