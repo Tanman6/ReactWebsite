6b6461c71c314d7756c6af85be2bbf0295a9e8a5
Tanman6 <tanmansyx@protonmail.com>
2022-04-06 15:59:57 -0500

    updated navbar to underline/highlite active router link

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Button } from "react-bootstrap"
+import { Button } from "react-bootstrap";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
@@ -11,10 +11,10 @@ const LoginButton = () => {
     onClick={() => loginWithRedirect()}
       id="qsLoginBtn"  
       variant="primary"
-      className="btn-margin"
+      className="fancy-border"
      
     >
-      Log In
+      <div>Log In</div>
     </Button>
   );
 };
